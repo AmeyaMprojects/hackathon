869251e85c00d22713d2aa332284e57ccd752f16
Ameya Mhatre <ameyam.projects@gmail.com>
2025-06-13 09:33:15 +0530

    m

--- hackathon-page.tsx
+++ hackathon-page.tsx
@@ -87,10 +87,10 @@ export default function HackathonPage() {
                 <Users className="w-5 h-5" />
                 <span>2 people per team</span>
               </div>
-              <div className="flex items-center space-x-2">
+              {/* <div className="flex items-center space-x-2">
                 <Trophy className="w-5 h-5" />
                 <span>₹XXXXK in Prizes</span>
-              </div>
+              </div> */}
               <div className="flex items-center space-x-2">
                 <Clock className="w-5 h-5" />
                 <span>MultiRound hackathon</span>
